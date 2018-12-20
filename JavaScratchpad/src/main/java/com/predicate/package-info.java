5e62573e8fa2dd95833b7845034954c234a5eeaa
kaliyugantagonist <kaliyugantagonist@gmail.com>
2018-12-20 15:11:54 +0100

    Introduction to Generics, Predicate

---
+++ JavaScratchpad/src/main/java/com/predicate/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Demonstrates the usage of the java.util.function.Predicate interface.
+ * 
+ * Notes:
+ * - To create and use the Predicate interface, it is NOT essential to use lambda expressions. Get the concept first
+ * - It helps if you understand a bit of mathematical concept of predicate
+ * - Put in plain English(from Schildt's book) a Predicate<T>:
+ determines if an object of type T fulfills some constraint. Returns a boolean value that indicates the outcome. Its method is called test().
+ */
+/**
+ * @author kaliyugantagonist
+ *
+ */
+package com.predicate;
---
+++ JavaScratchpad/src/test/java/com/generics/BasicGenericsDemonstratorTester.java
@@ -0,0 +1,47 @@
+package com.generics;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Pay attention to the comments!!!
+ * 
+ * @author kaliyugantagonist
+ *
+ */
+public class BasicGenericsDemonstratorTester {
+	
+	Logger logger = LoggerFactory.getLogger(BasicGenericsDemonstratorTester.class);
+
+	@Test
+	public void testPrintTypeClass() {
+		BasicGenericsDemonstrator<String, Integer> basicGenericsDemonstrator = new BasicGenericsDemonstrator<String, Integer>(
+				"John", 30);
+		basicGenericsDemonstrator.printTypeClass();
+	}
+
+	@Test
+	public void testGetTInstance() {
+		BasicGenericsDemonstrator<String, Integer> basicGenericsDemonstrator = new BasicGenericsDemonstrator<String, Integer>(
+				"John", 30);
+		/*Note: This not only compiles but also a cast isn't required.*/
+		String tRef = basicGenericsDemonstrator.getTInstance();
+		
+		if(tRef != null && !tRef.isEmpty()) {
+			logger.debug("String length is {}", tRef.length());
+		}
+	}
+
+	@Test
+	public void testGetVInstance() {
+		BasicGenericsDemonstrator<String, Integer> basicGenericsDemonstrator = new BasicGenericsDemonstrator<String, Integer>(
+				"John", 30);
+		/*Note: This not only compiles but also a cast isn't required.*/
+		Integer vRef = basicGenericsDemonstrator.getVInstance();
+		if(vRef != null) {
+			logger.debug("Int value is {}", vRef.intValue());
+		}
+	}
+
+}
